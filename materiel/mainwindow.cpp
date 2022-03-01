cf6f5daf88c38c361eada4f7c4843b0bf8400680
YosrBennagra <80179447+YosrBennagra@users.noreply.github.com>
2022-03-01 11:11:30 +0100

    crud

---
+++ materiel/mainwindow.cpp
@@ -0,0 +1,91 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QIntValidator>
+#include <QtDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->tableView->setModel(m->afficher());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+
+void MainWindow::on_ButtonAjouter_2_clicked()
+{
+    QString reference=ui->ReferenceLE->text();
+    QString nom=ui->nomLE->text();
+    QString matricule=ui->MatriculeLE->text();
+    QString idfournisseur="1";
+    int quantite=ui->ReferenceLE_4->text().toInt();
+    int prix=ui->ReferenceLE_2->text().toInt();
+    int etat=1;
+    materiel m(reference,nom,matricule,idfournisseur,quantite,prix,etat);
+    bool test=m.ajouter();
+    if(test)     {
+         ui->tableView->setModel(m.afficher());
+        QMessageBox::information(nullptr, QObject::tr("OK"),
+                                 QObject::tr("Ajout effectué\n"
+                                             "Click to Cancel."), QMessageBox::Cancel);
+    }     else
+        QMessageBox::information(nullptr, QObject::tr("not OK"),
+                                 QObject::tr("Ajout non effectué\n"
+                                             "Click to Cancel."), QMessageBox::Cancel);
+
+}
+
+void MainWindow::on_ButtonSupprimer_clicked()
+{
+
+    QString reference=ui->lineEdit->text();
+
+    bool test=m->supprimer(reference);
+
+    if(test)
+    {
+        ui->tableView->setModel(m->afficher());
+        QMessageBox::information(nullptr,QObject::tr("OK"),
+                                 QObject::tr("Suppression effectué\n"
+                                             "Click Cancel to Exit."),QMessageBox::Cancel);
+    }
+    else
+        QMessageBox::critical(nullptr,QObject::tr("not OK"),
+                                 QObject::tr("Suppression non effectué\n"
+                                             "Click Cancel to Exit."),QMessageBox::Cancel);
+}
+
+void MainWindow::on_ButtonTri_clicked()
+{
+    ui->tableView->setModel(m->afficher1());
+}
+
+void MainWindow::on_ButtonAjouter_4_clicked()
+{
+    QString reference=ui->ReferenceLE->text();
+    QString nom=ui->nomLE->text();
+    QString matricule=ui->MatriculeLE->text();
+    QString idfournisseur="1";
+    int quantite=ui->ReferenceLE_4->text().toInt();
+    int prix=ui->ReferenceLE_2->text().toInt();
+    int etat=1;
+
+   bool test =  m->modifier(reference,nom,matricule,idfournisseur,quantite,prix,etat);
+
+    if(test){
+        ui->tableView->setModel(m->afficher());
+        QMessageBox::information(nullptr, QObject::tr("OK"),
+        QObject::tr("update effectué\n""Click to Cancel."),
+                                 QMessageBox::Cancel);     }
+    else         QMessageBox::critical  (nullptr, QObject::tr("not OK"),
+                 QObject::tr("update non effectué\n""Click to Cancel."), QMessageBox::Cancel);
+}
---
+++ materiel/materiel.h
@@ -0,0 +1,37 @@
+#ifndef MATERIEL_H
+#define MATERIEL_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+
+class materiel
+{
+    QString reference,nom,matricule,idfournisseur;
+    int quantite,prix,etat;
+public:
+    materiel();
+    materiel(QString reference,QString nom,QString matricule,QString idfournisseur,int quantite,int prix,int etat);
+    QSqlQueryModel * afficher();
+    QSqlQueryModel * afficher1();
+    bool ajouter();
+    bool supprimer(QString reference);
+    bool modifier(QString reference,QString nom,QString matricule,QString idfournisseur,int quantite,int prix,int etat);
+//getters
+int getQuantite(){return quantite;}
+int getPrix(){return prix;}
+int getEtat(){return etat;}
+QString getReference(){return reference;}
+QString getNom(){return nom;}
+QString getMatricule(){return  matricule;}
+QString getIdfournisseur(){return idfournisseur;}
+//setters
+void setQuantite(int q){quantite=q;}
+void setPrix(int pt){prix=pt;}
+void setEtat(int et){etat=et;}
+void setReference(QString r){reference=r;}
+void setNom(QString n){nom=n;}
+void setMatricule(QString mat){matricule=mat;}
+void setIdfournisseur(QString id){idfournisseur=id;}
+
+};
+#endif // MATERIEL_H
